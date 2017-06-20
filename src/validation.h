40d0c7161da9db95e1c3691bf2666859e0b2dd50
Luke Dashjr <luke-jr+git@utopios.org>
2017-06-20 05:20:00 +0000

    Set -bip148=1 by default

--- src/validation.h
+++ src/validation.h
@@ -76,7 +76,7 @@ static const unsigned int BLOCKFILE_CHUNK_SIZE = 0x1000000; // 16 MiB
 /** The pre-allocation chunk size for rev?????.dat files (since 0.8) */
 static const unsigned int UNDOFILE_CHUNK_SIZE = 0x100000; // 1 MiB
 /** Default for -bip148 enforcement */
-static const unsigned int DEFAULT_BIP148 = false;
+static const unsigned int DEFAULT_BIP148 = true;
 
 /** Maximum number of script-checking threads allowed */
 static const int MAX_SCRIPTCHECK_THREADS = 16;
